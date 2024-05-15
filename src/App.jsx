0796270df33e3e939c98c74d2b1a1a0cfaabd645
Casiano <casianoalmeida2004@hotmail.com>
2024-05-14 22:28:23 -0300

    Commit de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,34 @@ import Home from "./Pages/Home/Home"
 import SignIn from "./Pages/Signin/Signin";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from "./Pages/Signup/Signup";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 
 function App() {
-  
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: (
+        <Home />
+      ),
+    },
+    {
+      path: "/SignIn",
+      element: (
 
-  return (
-    <>
-{/*       <Home/>
- */}  
-    {/* <SignIn/> */}
-  
-  <SignUp/> 
-  
-  </>
+        <SignIn />
 
-  )
+      ),
+    },
+    {
+      path: "/SignUp",
+      element: (
+        <SignUp />
+      ),
+    }
+
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
