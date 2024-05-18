e3f23761b608c378efd25bf22b62b4ec9ad68911
Nico Villalba <nicolasvillalba3@gmail.com>
2024-05-18 18:50:41 -0300

    Ingreso de datos

--- src/Components/ContentSuperAdmin/ContentSuperAdmin.jsx
+++ src/Components/ContentSuperAdmin/ContentSuperAdmin.jsx
@@ -5,14 +5,28 @@ const ContentSuperAdmin = () => {
 
     const mostrarDialogo = () => {
         Swal.fire({
-            title: 'Ingrese un valor:',
-            input: 'text',
+            title: 'Ingrese datos a cargar:',
+            html:
+                '<input id="input1" class="swal2-input" placeholder="Nombre">' +
+                '<input id="input2" class="swal2-input" placeholder="Género">' +
+                '<input id="input3" class="swal2-input" placeholder="Rating">' +
+                '<input id="input4" class="swal2-input" placeholder="Url">' +
+                '<input id="input5" class="swal2-input" placeholder="Peli/Serie">' +
+                '<input id="input6" class="swal2-input" placeholder="Año lanzamiento">',
             showCancelButton: true,
             confirmButtonText: 'Agregar',
             cancelButtonText: 'Cancelar',
-            preConfirm: (valor) => {
-                // Aquí puedes hacer algo con el valor ingresado, por ejemplo, enviarlo a una función de manejo
-                console.log('Valor ingresado:', valor);
+            preConfirm: () => {
+                // Obtener los valores de los inputs
+                const input1 = document.getElementById('input1').value;
+                const input2 = document.getElementById('input2').value;
+                const input3 = document.getElementById('input3').value;
+                const input4 = document.getElementById('input4').value;
+                const input5 = document.getElementById('input5').value;
+                const input6 = document.getElementById('input6').value;
+
+                // Aquí puedes hacer algo con los valores ingresados, por ejemplo, enviarlos a una función de manejo
+                console.log('Valores ingresados:', input1, input2, input3, input4);
             }
         });
     };
--- src/Pages/SuperAdmin/SuperAdmin.jsx
+++ src/Pages/SuperAdmin/SuperAdmin.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Footer from "../../Components/Footer/Footer"
 import NavBarPage from '../../Components/Navbar/Navbar'
+import ContentSuperAdmin from '../../Components/ContentSuperAdmin/ContentSuperAdmin'
 
 
 const SuperAdmin = () => {
   return (
     <div>
         <NavBarPage/>
+        <ContentSuperAdmin/>
         <Footer/>
     </div>
   )
