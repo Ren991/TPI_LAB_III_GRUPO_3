3cbf53233f010888eeea555b0d7d7e4c89102b32
Ren991 <beccrenzo@gmail.com>
2024-06-12 22:50:39 -0300

    fix error en admin

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
-import Footer from "../../Components/Footer/Footer"
-import NavBarPage from '../../Components/Navbar/Navbar'
-import ContentAdmin from '../../Components/ContentAdmin/ContentAdmin'
-import {useUser} from "../../Components/AuthContext/AuthContext";
-import {useEffect} from "react";
+import React, { useEffect, useState } from 'react';
+import Footer from "../../Components/Footer/Footer";
+import NavBarPage from '../../Components/Navbar/Navbar';
+import ContentAdmin from '../../Components/ContentAdmin/ContentAdmin';
+import { useUser } from "../../Components/AuthContext/AuthContext";
 import { useNavigate } from 'react-router-dom';
 
-
 const Admin = () => {
-
-  const {user} = useUser(); 
+  const { user } = useUser(); 
   const navigate = useNavigate();
-  
-  useEffect(() => {
-    console.log(user);
-    if (user &&user.role && user.role !==  "admin") {
-     navigate("/home")
-    }
-    
-    console.log(user);
-  }, [user]);     
+  const [loading, setLoading] = useState(true);
 
- 
+  useEffect(() => {
+    if (user && user.role !== "admin"){
+      navigate("/home");
+    }
+  }, [user]);
+
+
   return (
-    <div style={{width: '100%'}}>
-        <NavBarPage/>
-        <ContentAdmin/>
-        <Footer/>
+    <div style={{ width: '100%' }}>
+      <NavBarPage />
+      <ContentAdmin />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
