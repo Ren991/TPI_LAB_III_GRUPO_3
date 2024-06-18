b608cd5a81336b2582c1eda87653e24f073b67a8
Casiano <casianoalmeida2004@hotmail.com>
2024-06-18 20:23:03 -0300

    agregar admin

--- src/Components/ContentSuperAdmin/ContentSuperAdmin.jsx
+++ src/Components/ContentSuperAdmin/ContentSuperAdmin.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import BoardMovieSerie from '../BoardMovieSerie/BoardMovieSerie';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '../../Services/firebase';
+import { useNavigate } from "react-router-dom";
+
 
 const ContentSuperAdmin = () => {
-
+    const navigate = useNavigate();
+    const auth = getAuth();
     const AddMovie = () => {
         Swal.fire({
             title: 'Ingrese datos a cargar:',
@@ -37,21 +43,52 @@ const ContentSuperAdmin = () => {
         Swal.fire({
             title: 'Ingrese admin nuevo:',
             html:
-                '<input id="input1" class="swal2-input" placeholder="Nombre">' +
-                '<input id="input2" class="swal2-input" placeholder="Contraseña">',
-              
+                '<input id="input1" class="swal2-input" placeholder="Email">' +
+                '<input id="input2" class="swal2-input" placeholder="Username">'+
+                '<input id="input3" class="swal2-input" placeholder="Contraseña">',
+                
+            
             showCancelButton: true,
             confirmButtonText: 'Agregar',
             cancelButtonText: 'Cancelar',
-            preConfirm: () => {
+            preConfirm: async() => {
                 // Obtener los valores de los inputs
                 const input1 = document.getElementById('input1').value;
                 const input2 = document.getElementById('input2').value;
-
+                const input3 = document.getElementById('input3').value;
+                
+                
+                try {
+                    const userCredential = await createUserWithEmailAndPassword(auth, input1, input3);
+                    console.log(userCredential);
+                    const user = userCredential.user;
+                    console.log(user);
+                    const userDocRef = doc(db, 'users', user.uid);
+                    await setDoc(userDocRef, {
+                        email: input1,
+                        role: 'admin',
+                        username: input2,
+                        favorites: [] //para guardar los favoritos.
+                    });
+        
+                    console.log('User signed up and role set:', user);
+                    navigate(("/signin"));
+                } catch (error) {
+                    console.error('Error signing up:', error);
+                    
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'Error al registrar un admin, intentente nuevamente.',
+                        icon: 'error',
+                        confirmButtonText: 'Salir'
+                    })
+                }
 
                 // Aquí puedes hacer algo con los valores ingresados, por ejemplo, enviarlos a una función de manejo
                 console.log('Valores ingresados:', input1, input2);
             }
+
+
         });
     };
 
