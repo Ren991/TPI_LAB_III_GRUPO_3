847bb626daec6f134e08f06e895d94e013b48804
Casiano <casianoalmeida2004@hotmail.com>
2024-06-18 21:15:41 -0300

    remover admins

--- src/Components/ContentSuperAdmin/ContentSuperAdmin.jsx
+++ src/Components/ContentSuperAdmin/ContentSuperAdmin.jsx
@@ -1,42 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import BoardMovieSerie from '../BoardMovieSerie/BoardMovieSerie';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
-import { db } from '../../Services/firebase';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { doc, setDoc, getFirestore, collection, query, where, getDocs, deleteDoc } from 'firebase/firestore';
+import { auth, db } from '../../Services/firebase';
 import { useNavigate } from "react-router-dom";
 
-
 const ContentSuperAdmin = () => {
+    const [admins, setAdmins] = useState([]);
     const navigate = useNavigate();
-    const auth = getAuth();
-    const AddMovie = () => {
-        Swal.fire({
-            title: 'Ingrese datos a cargar:',
-            html:
-                '<input id="input1" class="swal2-input" placeholder="Nombre">' +
-                '<input id="input2" class="swal2-input" placeholder="Género">' +
-                '<input id="input3" class="swal2-input" placeholder="Rating">' +
-                '<input id="input4" class="swal2-input" placeholder="Url">' +
-                '<input id="input5" class="swal2-input" placeholder="Peli/Serie">' +
-                '<input id="input6" class="swal2-input" placeholder="Año lanzamiento">',
-            showCancelButton: true,
-            confirmButtonText: 'Agregar',
-            cancelButtonText: 'Cancelar',
-            preConfirm: () => {
-                // Obtener los valores de los inputs
-                const input1 = document.getElementById('input1').value;
-                const input2 = document.getElementById('input2').value;
-                const input3 = document.getElementById('input3').value;
-                const input4 = document.getElementById('input4').value;
-                const input5 = document.getElementById('input5').value;
-                const input6 = document.getElementById('input6').value;
 
-                // Aquí puedes hacer algo con los valores ingresados, por ejemplo, enviarlos a una función de manejo
-                console.log('Valores ingresados:', input1, input2, input3, input4, input5, input6);
-            }
-        });
+    useEffect(() => {
+        fetchAdminUsers();
+    }, []);
+
+    const fetchAdminUsers = async () => {
+        const adminsCollection = collection(db, 'users');
+        const q = query(adminsCollection, where("role", "==", "admin"));
+        try {
+            const querySnapshot = await getDocs(q);
+            const adminList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setAdmins(adminList);
+        } catch (error) {
+            console.error("Error fetching admin users:", error);
+        }
     };
 
     const AddAdmin = () => {
@@ -52,7 +40,7 @@ const ContentSuperAdmin = () => {
             confirmButtonText: 'Agregar',
             cancelButtonText: 'Cancelar',
             preConfirm: async() => {
-                // Obtener los valores de los inputs
+                
                 const input1 = document.getElementById('input1').value;
                 const input2 = document.getElementById('input2').value;
                 const input3 = document.getElementById('input3').value;
@@ -68,7 +56,7 @@ const ContentSuperAdmin = () => {
                         email: input1,
                         role: 'admin',
                         username: input2,
-                        favorites: [] //para guardar los favoritos.
+                        favorites: [] 
                     });
         
                     console.log('User signed up and role set:', user);
@@ -83,50 +71,102 @@ const ContentSuperAdmin = () => {
                         confirmButtonText: 'Salir'
                     })
                 }
-
-                // Aquí puedes hacer algo con los valores ingresados, por ejemplo, enviarlos a una función de manejo
+    
+                
                 console.log('Valores ingresados:', input1, input2);
             }
-
-
+    
+    
         });
     };
 
     const RemoveAdmin = () => {
         Swal.fire({
-            title: 'Ingrese nombre:',
-            html:
-                '<input id="input1" class="swal2-input" placeholder="Admin a eliminar">',
-              
+            title: 'Lista de Admins',
+            html: createAdminTable(),
             showCancelButton: true,
-            confirmButtonText: 'Eliminar',
-            cancelButtonText: 'Cancelar',
-            preConfirm: () => {
-                // Obtener los valores de los inputs
-                const input1 = document.getElementById('input1').value;
-
-
-                // Aquí puedes hacer algo con los valores ingresados, por ejemplo, enviarlos a una función de manejo
-                console.log('Valores ingresados:', input1);
-            }
+            cancelButtonText: 'Cerrar',
+            showConfirmButton: false
         });
     };
 
+    const createAdminTable = () => {
+        let tableHtml = '<table class="table"><thead><tr><th>Email</th><th>UID</th><th>Acción</th></tr></thead><tbody>';
+        admins.forEach(admin => {
+            tableHtml += `<tr>
+                <td>${admin.email}</td>
+                <td>${admin.id}</td>
+                <td><button class="btn btn-danger" onclick="deleteAdmin('${admin.id}')">Eliminar</button></td>
+            </tr>`;
+        });
+        tableHtml += '</tbody></table>';
+        return tableHtml;
+    };
 
+    window.deleteAdmin = async (uid) => {
+        const confirmation = await Swal.fire({
+            title: "Estas seguro?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, Eliminar!"
+        });
+
+        if (confirmation.isConfirmed) {
+            try {
+                await deleteUserFromAuth(uid);
+                await deleteUserFromFirestore(uid);
+                fetchAdminUsers(); // Actualizar la lista de admins después de eliminar uno
+                Swal.fire('Eliminado!', 'El admin ha sido eliminado.', 'success');
+            } catch (error) {
+                console.error('Error in user deletion process:', error);
+                Swal.fire('Error', 'Hubo un problema al eliminar el admin.', 'error');
+            }
+        }
+    };
+
+    const deleteUserFromAuth = async (uid) => {
+        try {
+           // verificar permisos para eliminar del servicio de autenticación
+        } catch (error) {
+            console.error('Error deleting user from auth:', error);
+        }
+    };
+
+    const deleteUserFromFirestore = async (uid) => {
+        try {
+            const userDocRef = doc(db, 'users', uid);
+            await deleteDoc(userDocRef);
+            console.log('Successfully deleted user document with uid: ${uid}');
+        } catch (error) {
+            console.error('Error deleting user from Firestore:', error);
+        }
+    };
 
     return (
-        <div style={{width: '70%', marginLeft: 'auto', marginRight: 'auto', marginTop: '20px'}}>
-            
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-                {/* <Button onClick={AddMovie}>Agregar Película / Serie</Button> */}
+        <div style={{ width: '70%', marginLeft: 'auto', marginRight: 'auto', marginTop: '20px' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
                 <Button onClick={AddAdmin}>Agregar Admin</Button>
                 <Button onClick={RemoveAdmin}>Eliminar Admin</Button>
             </div>
             <div>
-                <BoardMovieSerie/>
+                <BoardMovieSerie />
+            </div>
+            <div style={{ marginTop: '20px' }}>
+                <h3>Lista de Admins</h3>
+                <ul>
+                    {admins.map(admin => (
+                        <li key={admin.id}>
+                            {admin.username} ({admin.email})
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
 }
 
-export default ContentSuperAdmin
+export default ContentSuperAdmin;
+
+
