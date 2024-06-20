4366d8bbb10a9fa3590cc1b0521a1618087e94d1
Ren991 <beccrenzo@gmail.com>
2024-06-19 21:37:02 -0300

    página home y estado general de movies

--- src/Components/ContentHome/ContentHome.jsx
+++ src/Components/ContentHome/ContentHome.jsx
@@ -1,10 +1,34 @@
 import News from "../News/News"
 import Trends from "../Trends/Trends"
 import Image from 'react-bootstrap/Image';
+import React, { useContext, useState,useEffect } from 'react';
+
+import { MoviesContext } from "../MovieContext/MovieContext";
 
 
 
 function ContentHome() {
+  const { movies } = useContext(MoviesContext);
+
+  const [trends, setTrends] = useState([]);
+  const [news, setNews] = useState([]);
+  
+  
+
+  useEffect(() => {
+    if (movies.length > 0) {
+        // Ordenar películas por rating en orden descendente y obtener las primeras 4
+        const topRatedMovies = [...movies].sort((a, b) => b.rating - a.rating).slice(0, 4);
+        setTrends(topRatedMovies);
+
+        // Ordenar películas por fecha de lanzamiento en orden descendente y obtener las más recientes
+        const recentMovies =  [...movies].sort((a, b) => b.anioLanzamiento - a.anioLanzamiento).slice(0, 4);
+        setNews(recentMovies);
+    }
+}, [movies]);
+console.log(news);
+console.log(trends);
+
 
   return (
     <div>
@@ -13,9 +37,9 @@ function ContentHome() {
       <div style={{width:"80%", marginLeft:"auto", marginRight:"auto"}}>
       
         <h2>Tendencias</h2>
-        <Trends/>
+        <Trends trends={trends}/>
         <h2>Novedades</h2>
-        <News/>
+        <News news={news}/>
       </div>
 
     </div>
--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-function News() {
-    const movies = [
-        {
-            title: "Interstellar",
-            imageUrl: "https://m.media-amazon.com/images/S/pv-target-images/79194981293eabf6620ece96eb5a9c1fffa04d3374ae12986e0748800b37b9cf.jpg"
-        },
-        {
-            title: "The Shawshank Redemption",
-            imageUrl: "https://m.media-amazon.com/images/S/pv-target-images/79194981293eabf6620ece96eb5a9c1fffa04d3374ae12986e0748800b37b9cf.jpg"
-        },
-        {
-            title: "Inception",
-            imageUrl: "https://m.media-amazon.com/images/S/pv-target-images/79194981293eabf6620ece96eb5a9c1fffa04d3374ae12986e0748800b37b9cf.jpg"
-        },
-        {
-            title: "The Dark Knight",
-            imageUrl: "https://m.media-amazon.com/images/S/pv-target-images/79194981293eabf6620ece96eb5a9c1fffa04d3374ae12986e0748800b37b9cf.jpg"
-        }
-    ];
-
+function News({news}) {
+    
     return (
         <div style={{display:"flex", justifyContent:"space-around",flexWrap:"wrap"}}>
-            {movies.map((movie, index) => (
+            {news.map((movie, index) => (
                 <Card key={index} style={{ width: '18rem', marginTop:"12px" }}>
-                    <Card.Img variant="top" src={movie.imageUrl} />
+                    <Card.Img variant="top" src={movie.urlImagen} />
                     <Card.Body>
-                        <Card.Title>{movie.title}</Card.Title>
-                       
-                        <Button variant="primary">Go somewhere</Button>
+                        <Card.Title>{movie.nombre}</Card.Title>
+                        <Card.Subtitle>{movie.anioLanzamiento}</Card.Subtitle>
+                        <Button variant="primary">Reproducir</Button>
                     </Card.Body>
                 </Card>
             ))}
