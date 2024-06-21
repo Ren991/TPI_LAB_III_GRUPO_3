ab494d1bbc786e4cb2d703f59a96c44352a73c8b
Nico Villalba <nicolasvillalba3@gmail.com>
2024-06-21 00:13:42 -0300

    reproductor funcional

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,20 +1,37 @@
 import { Button } from "react-bootstrap"
-
-
+import { useParams } from "react-router-dom"
+import { MoviesContext } from "../MovieContext/MovieContext";
+import { useContext } from "react";
 
 
 function VideoPlayer() {
+  const {id} = useParams();
+  const { movies } = useContext(MoviesContext);
+  const movie = movies.find(movie => movie.id === id);
 
+  if (!movie) {
+    return <div>Película no encontrada</div>;
+  }
+  console.log(movie);
+
+  console.log(movie.urlVideo);
+ 
   return (
-    <div style={{width:"80%",marginLeft:"auto",marginRight:"auto"}}>
-        <div style={{marginTop:"25px"}}>
-            <h2>Titulo Película</h2>
-            <iframe width="100%" height="415" src="https://www.youtube.com/embed/vBdco69DSCA?si=nsI8apDp4Hw8GvUJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <Button>Añadir a favoritos</Button>
-        </div>     
+    <div style={{ width: "80%", marginLeft: "auto", marginRight: "auto" }}>
+      <div style={{ marginTop: "25px" }}>
+        <h2>{movie.title}</h2>
+        <iframe
+          width="100%"
+          height="415"
+          src={movie.urlVideo} // Usa la URL del video de la película encontrada
+          title={movie.nombre}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <Button>Añadir a favoritos</Button>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default VideoPlayer
+export default VideoPlayer;
